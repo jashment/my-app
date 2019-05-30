77e824524dfc81d3b8c19267f6f2d06392b40f9d
Jordan <jordanashment@gmail.com>
2019-05-30 15:29:08 +0000

    Intro to Hooks

--- src/App.js
+++ src/App.js
@@ -1,40 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const App = props => {
+  
+  const [personsState, setPersonsState] = useState({
     persons: [
       {name: 'Jordan', age: 28},
       {name: 'Amy', age: 27},
       {name: 'Eli', age: 0},
     ]
-  }
+  });
   
-  switchNameHandler = () => {
-    // console.log('Was Clicked');
-    this.setState({persons: [
-      {name: 'Jordan the Destroyer', age: 28},
-      {name: 'Amytha', age: 27},
-      {name: 'Baby', age: 0},
-    ]
-      
+  const [otherState, setOtherState] = useState('some other value');
+  
+  const switchNameHandler = () => {
+    setPersonsState({
+      persons: [
+        {name: 'Jordan the Destroyer', age: 28},
+        {name: 'Amytha', age: 27},
+        {name: 'Baby Eli', age: 0},
+        ]
     })
-  }
+  };
+
   
-  render() {
+  // render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Being Awesome</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={switchNameHandler}>Switch Name</button>
+        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My Hobbies: Being Awesome</Person>
+        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
+        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
       </div>
     );
 
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!'));
-  }
+  // }
 }
 
 export default App;
