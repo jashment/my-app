fda444d7e8c08dbcc64a5cf34f001ca623839cbf
Jordan <jordanashment@gmail.com>
2019-06-04 15:50:50 +0000

    Enabling CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 import Radium, {StyleRoot} from 'radium';
 
@@ -81,19 +81,19 @@ class App extends Component {
       }
     }
     
-    let classes = [];
+    let assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if(this.state.persons.length <=1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
     
     return (
       <StyleRoot>
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App!</h1>
-        <p className={classes.join(' ')}>I'm a Paragraph!</p>
+        <p className={assignedClasses.join(' ')}>I'm a Paragraph!</p>
         <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
       </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './Person.css';
+
 import Radium from 'radium';
+import classes from './Person.css'
 
 const person = (props) => {
     const style = {
@@ -9,7 +10,7 @@ const person = (props) => {
         }
     }
     return (
-        <div className="Person" style={style}>
+        <div className={classes.Person} style={style}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
