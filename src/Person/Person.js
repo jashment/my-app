8be78c0558e2171d99d08705faa57405b3b6ea89
Jordan <jordanashment@gmail.com>
2019-06-07 14:48:00 +0000

    Using Media Queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,13 +4,9 @@ import Radium from 'radium';
 import classes from './Person.css'
 
 const person = (props) => {
-    const style = {
-        '@media (min-width: 500px': {
-            width: '450px'
-        }
-    }
+
     return (
-        <div className={classes.Person} style={style}>
+        <div className={classes.Person} >
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
