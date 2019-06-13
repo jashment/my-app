a2ce0d19d574c32f02cdfda5cca0c6244c264416
Jordan <jordanashment@gmail.com>
2019-06-13 14:58:40 +0000

    Playing with lifecycle

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => 
-    props.persons.map((person, index) => {
+class Persons extends Component {
+static getDerivedStateFromProps(props, state) {
+    console.log('[Persons.js] getDerivedStateFromProps');
+    return state;
+}    
 
-            return <Person 
-            click={() => props.clicked(index)} 
+shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    
+    return true;
+}
+
+getSnapshotBeforeUpdate(prevProsp, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+}
+
+componentDidUpdate() {
+    console.log('[Persons.js] componentDidUpdate');
+}
+
+    render() {
+        return this.props.persons.map((person, index) => {
+
+        return <Person 
+            click={() => this.props.clicked(index)} 
             name={person.name} 
             age={person.age}
             key={person.id}
-            changed={(event) => props.changed(event, person.id)} />
+            changed={(event) => this.props.changed(event, person.id)} />
           });
+          
+    }
+}
 
-
-export default persons;
+export default Persons;
